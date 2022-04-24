7bd43eeb4969dc78b1c35581c1e1eb4965550051
Hrantaghayan <99826154+Hrantaghayan@users.noreply.github.com>
2022-04-24 12:36:15 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,128 +1,137 @@
-// homework 1 
-//  level1 xndir 1 Given an object. Invert it (keys become values and values become keys). If there is
-// more than key for that given value create an array.
-let obj = {
-        a:1,
-        b:1,
-        c:2,
-        e:4,
-        d:2,
-        h:3,
-        j:1,
-        h:3
+// 1. Given an array of numbers which is almost sorted in ascending order.  Find the index
+// where sorting order is violated.
+// 1tarberak
+function isviolated(arr){
+    let a;
+    for(let i = 0;i<arr.length;i++){
+       if(arr[i]>arr[i+1]){
+           return i+1
+       }
     }
-let result = {}
-for(let k in obj){
-    const key = obj[k].toString()
-    if(result[key]){
-      if(Array.isArray(result[key])){
-          result[key].push(k)
-      }
-      else{
-          let array = []
-          array.push(result[key])
-          array.push(k)
-          result[key]=array
-      }
-    }
-    else{
-        result[key] = k
-    }
+    return -1
 }
-console.log(obj)
+let arr = [1,2,3,4,5,8,6]
+console.log(isviolated(arr))
+
+// erkrord tarberak
+let arr = [1,2,3,4,5,8,6]
+const index = arr.findIndex(function (el,index,arr){
+    if(el<arr[index - 1]){
+        return true
+    }
+    return false
+}) 
+console.log(index)
+
+// 2. Given an array consisting from the arrays of numbers (like a two-dimensional array).
+// Find sum of each row and print them as an array
+// 1 tarberak 
+function arraypoqracnel(arr){
+    for(let i = 0;i<arr.length;i++){
+        let sum = 0
+        if(Array.isArray(arr[i])){
+            for(let j = 0;j<arr[i].length;j++){
+                 sum = sum + arr[i][j]
+            }
+            arr[i] = sum
+        }
+    }
+    return arr
+}
+let arr = [[1], [2], [3], [4]]
+console.log(arraypoqracnel(arr))
+
+// 2tarberak
+let arr = [[1], [2], [3], [4]]
+const havaqarray = arr.map(function (el,index,arr){
+    if(Array.isArray(el)){
+        let sum = 0
+        for(let i = 0;i<el.length;i++){
+            sum = sum + el[i]
+        }
+        return sum
+    }
+})
+console.log(havaqarray)
+
+// 3. Given an array of integers. Write a function that return new array from first array,
+// where removed even numbers, and odd numbers was multiplied with new array length
+// 1tarberak
+function  arraymecacnelerkarutyamb(arr){
+    let result = []
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i]%2 !== 0){
+            result.push(arr[i])
+        }
+    }
+    for(let j = 0;j<result.length;j++){
+        result[j] = result[j]*result.length
+    }
+    return result
+}
+let arr = [[]]
+console.log(arraymecacnelerkarutyamb(arr))
+
+// erkrordtarberak
+let arr = [[]]
+const filter = arr.filter(function (el,index,arr){
+    if(el%2 !== 0){
+        return true
+    }
+})
+const result = filter.map(function (el,index,filter){
+    return el * filter.length
+})
 console.log(result)
 
-// xndir 2 Given two objects. Write a function that performs shallow compare.
-let a = {
-    a:1,
-    b:2
-}
-let b = {
-    a:1,
-    b:2,
-    c:3
-}
-function shalowcompare(obj1,obj2){
-    debugger
-    for(let key in obj1)
-     if(obj1[key] !== obj2[key] || !(key in obj2)){
-         return false
-    }
-    for(let key in obj2){
-        if(obj2[key]!== obj1[key] || !(key in obj1)){
-            return false
+// 4. Given a number. Write a function that calculates its sum of the digits and if that sum
+// has more than 1 digit find the sum of digits of that number. Repeat that process if needed and
+// return the result.
+// 1 tarberak
+function sumofdigits(number){
+    let sum = 0
+    while(number !== 0 || sum>=10){
+        if(number === 0 ){
+            number = sum
+            sum = 0
         }
+        let c = number%10
+        number = (number - c)/10
+        sum = sum + c
     }
-    return true
+    return sum
 }
-console.log(shalowcompare(a,b))
+console.log(sumofdigits(999999999999))
 
-// xnir 3 Given an array. Determine whether it consists only from uniques or not.
-function isunique(array){
-    let a;
-    if(array.length === 0){
-        return false;
+// 2tarberak
+function sumofdigits(number){
+    let sum = 0
+    while(number!==0){
+       let c = number%10
+       number = (number - c)/10
+       sum = sum + c
     }
-    for(let i = 0;i < array.length; i++){
-        a = array[i]
-        for(let j = i+1; j < array.length; j++){
-            if(a === array[j]){
-                return false
-            }
-        }
+    if(sum>=10||sum<=-10){
+        return sumofdigits(sum)
     }
-    return true
+    return sum
 }
-let arraylength = +prompt('enter a number for arrayss lebgth')
-let array = []
-for (let i = 0;i<arraylength;i++){
-array.push(prompt('enter anything'))
-}
-console.log(isunique(array))
+console.log(sumofdigits(999999999999))
 
-
-// 4 xndirDetermine if a word or phrase is an isogram. An isogram (also known as a &quot;non pattern
-// word&quot;) is a word or phrase without a repeating letter.
-function isIsogram(string){
-    let str = string.toLowerCase()
-    let firstcharacter;
-    for(let i = 0; i < str.length; i++){
-        firstcharacter = str[i]
-        for(let j = i+1; j<str.length; j++){
-            if(firstcharacter === str[j]){
-                console.log('your string is not isogram')
-                return false;
-            }
-        }
+// 5. Write a recursive function which receives  a number as arguments and returns the
+// fibonacci sequence as array.
+function finonachi(number){
+    let result = [1,1]
+    if(number === 0){
+        return []
     }
-    console.log('your string is isogram')
-    return true;
+    if(number === 1){
+        return [1]
+    }
+    for(let i = 2;i<number;i++){
+        let fn =result[i-1]+result[i-2]
+        result.push(fn)
+    }
+    return result
 }
-let string = prompt('enter your word')
-console.log(isIsogram(string))
-
-// xndir 5Given an array of integers. All numbers are unique. Find the count of missing numbers
-// between minimum and maximum elements to make integers sequence.
-function  countmissinnumbers(array){
-    let allcounts = 0
-    let missingcounts = 0
-    let max = -Infinity
-    let min = Infinity
-    for(let i = 0;i<array.length;i++){
-        if(min>array[i]){
-        min = array[i]
-        }
-    }
-    for(let i = 0;i<array.length;i++){
-        if(max<array[i]){
-            max = array[i]
-        }
-    }
-    for(let i = min;i<=max;i++){
-          allcounts++
-    }
-    missingcounts = allcounts - array.length 
-   return missingcounts
-}
-let array = [-5,-2,-1,1,0,2,6,9]
-console.log(countmissinnumbers(array))
+console.log(finonachi(10))
