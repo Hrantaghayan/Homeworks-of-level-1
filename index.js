bc4436f743951f622f0b2ff056befa883643272d
Hrantaghayan <99826154+Hrantaghayan@users.noreply.github.com>
2022-05-16 00:20:27 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,78 +1,108 @@
-// homework 3 
-// 1 Given an array. Write a recursive function that removes the first element and returns the given
-// array. (without using arr.unshift(),assign second element to first, third element to second...)
-function removefirstelement(arr,i){
-    debugger
-    arr[i] = arr[i+1]
-    i++
-if(i>=arr.length){
-    arr.pop()
-    return arr
-}
-return removefirstelement(arr,i)
-}
-function foruser(arr){
-    return removefirstelement(arr,0)
-}
-
-// 2Given an array of nested arrays. Write a recursive function that flattens it. (Hint create function
-// that concats arrays).
-function flatternarr(arr){
-    debugger
-    let result = []
-    for(let i = 0;i<arr.length;i++){
-        if(Array.isArray(arr[i])){
-        result = result.concat(flatternarr(arr[i]))
+// homework4
+//1 Create a function that builds a tree like object given an array with object which
+// contains parent and id properties.
+let arr = [
+        {parent:null,id:0},
+        {parent:0,id:1},
+        {parent:0,id:2},
+        {parent:1,id:3},
+        {parent:1,id:4},
+        {parent:2,id:5},
+        {parent:4,id:6},
+    ]
+    function objtree(arr){
+        let sortarr = arr.sort(function (a,b){
+            if(a.id<b.id){
+                return - 1
+            }
+        })
+        let result = {}
+        for(let node of sortarr){
+            debugger
+            if(node.parent === null){
+                result[node.id] = {}
+            }
+            else{
+                let branch = findbranch(node)
+                let item1 = result 
+                for(let value of branch){
+                    item1 = item1[value.id]
+                }
+                item1[node.id] = {}
+            }
         }
-        else{
-            result.push(arr[i])
+        function findbranch(object){
+            debugger
+            let result = []
+            while(object.parent !== null){
+                let item2 = sortarr.find(function (el,index,sortarr){
+                    debugger
+                    return el.id === object.parent
+                })
+                object = item2
+                result.push(item2)
+            }
+            return result.reverse()
         }
+        return result
     }
-    return result
-}
-
-// 3Given an array and a number N.  Write a recursive function that rotates an array N places to the
-// left. (Hint: to add element to the beginning use arr.unshift())
-function rotateArray(arr,n){
+    console.log(objtree(arr))
+    
+//  3 Write a recursive function to determine whether all digits of the number are odd or
+// not.
+ function isOdd(number){
     debugger
-    if(n===0){
-        return arr
+    if(number % 2===0&&number !==0){
+        return false
     }
-    arr.push(arr.shift())
-    n--
-    return rotateArray(arr,n)
-}
-
-// 4Given the list of the following readers:
-// Output the books sorted by the percent in descending order which readStatus is true.
-const filter = arr.filter(function(el,index,arr){
-    if(el.readStatus===true){
+    if(number === 0){
         return true
     }
-})   
-const result = filter.sort(function(a,b){
-    if(a.percent>b.percent){
-        return -1
-    }
-})     
-
-// 5 Implement “map” method for plain objects (same as Array map method.)
-function custommapforobjects(fn){
-    debugger
-    if (typeof fn !== 'function'){
-        console.log(fn +' is not function')
-        return
-    }
-let result = {}
-for(let key in this){
-    if(fn(this[key],key,this)){
-    result[key] = fn(this[key],key,this)
-    }
-    if(fn(this[key],key,this)===undefined){
-        if(result[key]!== fn){
-            result[key] = undefined 
-        }
-    }
+    let c = number %10
+    number = (number -c)/10
+    return isOdd(number)
 }
-return result
+
+
+// 4Given an array of numbers. Write a recursive function to find its minimal positive element. (if
+// such element does not exist, return -1)․
+ function findMinel(arr,i=0,min = Infinity){
+  if(arr[i]<min&&arr[i]>=0){
+      min = arr[i]
+  }
+  if(i===arr.length&& min === Infinity){
+      return - 1
+  }
+  if(i===arr.length){
+      return min
+  }
+   i++
+  return findMinel(arr,i,min)
+}
+let arr = [56, -9, 87, -23, 0, -105, 55, 1]
+console.log(findMinel(arr))
+
+function getmin(...args){
+    debugger
+    let min = Infinity 
+    let sortedarr =  args.sort(function (a,b){
+        if(a<b){
+            return - 1
+        }
+    })
+  for(let i = 0;i<sortedarr.length;i++){
+      if(sortedarr[i]<0){
+      continue
+      }
+      else{
+          min = sortedarr[i]
+          break
+      }
+  }
+  if(min === Infinity){
+      return - 1
+  }
+  else{
+      return min
+  }
 }
